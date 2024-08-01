69cf1adc7f62ee7e2e2cee3bb48f4a0a55a9609d
Rabinkhulimuli <lovingrabin@gmail.com>
2024-08-01 02:45:16 -0700

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,21 +5,25 @@ require("dotenv").config();
 const app = express();
 const task = require("./router/task");
 import connectDB from './db/connectDb'
-
+const PORT=process.env.PORT 
+import {Request,Response} from 'express'
 app.use(cors({
   origin: 'http://localhost:5173', 
   credentials: true, 
 }))
 app.use(cookieParser())
 app.use(express.json());
-app.use("/user", task);
 
+app.use("/user", task);
+app.get('/health',(req:Request,res:Response)=> {
+  res.status(200).send({message:"health OK"})
+})
 const start = async () => {
   try {
     await connectDB(process.env.CONNECTION_STRING as string);
     console.log("Database connected successfully");
-    app.listen(5000, () => {
-      console.log("server started at localhost:5000");
+    app.listen(PORT, () => {
+      console.log(`server started at localhost:${PORT}`);
     });
   } catch (err) {
     console.log(err);
